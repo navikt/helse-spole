8467dc35993079f262ff04a4eda98f1569e642df
Håkon <hakon.arneng.holmstedt@nav.no>
2019-08-27 13:01:56 +0200

    legger til pom og en Application-fil

---
+++ src/main/kotlin/no/nav/helse/spole/Application.kt
@@ -0,0 +1,13 @@
+package no.nav.helse.spole
+
+import org.springframework.boot.Banner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication(scanBasePackages = ["no.nav.helse"])
+class Application
+fun main(args: Array<String>) {
+    runApplication<Application>(*args) {
+        setBannerMode(Banner.Mode.OFF)
+    }
+}
