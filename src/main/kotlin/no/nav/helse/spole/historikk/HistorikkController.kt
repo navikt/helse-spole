7581c5c358da7477446eaf4223045fcc9c0a8d6f
Håkon <hakon.arneng.holmstedt@nav.no>
2019-09-04 12:54:01 +0200

    controller virker, men takler ikke parameter enda

--- src/main/kotlin/no/nav/helse/spole/historikk/HistorikkController.kt
+++ src/main/kotlin/no/nav/helse/spole/historikk/HistorikkController.kt
@@ -3,6 +3,8 @@ package no.nav.helse.spole.historikk
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
 
@@ -10,9 +12,12 @@ import java.time.LocalDate
 class HistorikkController(@Autowired @Qualifier("infotrygd") val infotrygd: PeriodeKilde,
                           @Autowired @Qualifier("spa") val spa: PeriodeKilde) {
 
-    @GetMapping("/sykepengeperioder/{aktørid}")
-    fun hentPerioder(aktørId: AktørId, fom: LocalDate = LocalDate.now().minusYears(3)): Sykepengeperioder =
-        infotrygdPerioder(aktørId, fom).join(spaPerioder(aktørId, fom))
+    @GetMapping("/sykepengeperioder/{aktørId}")
+    fun hentPerioder(@PathVariable("aktørId") aktørId: AktørId,
+                     @RequestParam("fom", required = false) fom: LocalDate?): Sykepengeperioder {
+        val faktiskFom = fom ?: LocalDate.now().minusYears(3)
+        return infotrygdPerioder(aktørId, faktiskFom).join(spaPerioder(aktørId, faktiskFom))
+    }
 
     private fun infotrygdPerioder(aktørId: AktørId, fom: LocalDate) = infotrygd.perioder(aktørId, fom)
     private fun spaPerioder(aktørId: AktørId, fom: LocalDate) = spa.perioder(aktørId, fom)
