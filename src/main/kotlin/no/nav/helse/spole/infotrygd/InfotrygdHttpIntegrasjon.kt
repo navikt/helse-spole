71cbeb82dd8b4b56dc82365b35e77aa8682d18fe
Håkon <hakon.arneng.holmstedt@nav.no>
2019-09-13 10:51:57 +0200

    bruk resttemplate siden vi uansett ikke er særlig reactive

--- src/main/kotlin/no/nav/helse/spole/infotrygd/InfotrygdHttpIntegrasjon.kt
+++ src/main/kotlin/no/nav/helse/spole/infotrygd/InfotrygdHttpIntegrasjon.kt
@@ -4,37 +4,42 @@ import no.nav.helse.spole.appsupport.Azure
 import no.nav.helse.spole.historikk.Kilde
 import no.nav.helse.spole.historikk.Periode
 import no.nav.helse.spole.infotrygd.fnr.Fodselsnummer
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.client.RestTemplate
+import java.net.URI
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 @Component
-class InfotrygdHttpIntegrasjon(val azure: Azure,
-                               @Value("\$infotrygd.url") val infotrygdRestUrl: String) : InfotrygdIntegrasjon {
+class InfotrygdHttpIntegrasjon(
+    val azure: Azure,
+    @Value("\$infotrygd.url") val infotrygdRestUrl: String
+) : InfotrygdIntegrasjon {
 
     override fun forFnr(fnr: Fodselsnummer, fom: LocalDate): Collection<Periode> {
-        println("Henter perioder fra Infotrygd")
-        println("Henter Azure AD token")
         val token = azure.hentToken()
-        println("Azure AD token hentet")
 
-        val webClient = WebClient.builder().baseUrl(infotrygdRestUrl).build()
+        val headers = HttpHeaders()
+        headers.setBearerAuth(token.accessToken)
+        headers.accept.add(MediaType.APPLICATION_JSON)
+
         println("Kaller Infotrygd Sykepengeliste")
-        return webClient.get()
-            .uri("?fnr=$fnr&fraDato=0")
-            .accept(MediaType.APPLICATION_JSON)
-            .header("Authorization", "Bearer ${token.accessToken}")
-            .retrieve()
-            .bodyToMono(ITSykepenger::class.java).block()!!.sykemeldingsperiode.asPerioder()
+        val response = RestTemplate().exchange(
+            "$infotrygdRestUrl?fnr=$fnr&fraDato=0",
+            HttpMethod.GET,
+            HttpEntity<String>(headers),
+            ITSykepenger::class.java
+        )
+
+        return response.body.sykemeldingsperiode.asPerioder()
     }
 
 }
 
 private fun List<ITPeriode>.asPerioder(): Collection<Periode> = this.map { it.toPeriode() }
-
 private fun ITPeriode.toPeriode(): Periode = Periode(this.sykemeldtFom, this.sykemeldtTom, this.grad, Kilde.INFOTRYGD)
+private fun String.asURI() = URI(this)
