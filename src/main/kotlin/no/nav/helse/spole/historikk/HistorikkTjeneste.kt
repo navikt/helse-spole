402f8202199c75257ef44ed878d0212028206a1f
Håkon <hakon.arneng.holmstedt@nav.no>
2019-10-02 11:52:33 +0200

    parameter fraDato styrer datoen vi søker etter historikk fra

--- src/main/kotlin/no/nav/helse/spole/historikk/HistorikkTjeneste.kt
+++ src/main/kotlin/no/nav/helse/spole/historikk/HistorikkTjeneste.kt
@@ -4,17 +4,21 @@ import io.ktor.application.call
 import io.ktor.auth.authenticate
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
-import io.ktor.routing.get
 import io.ktor.routing.Routing
+import io.ktor.routing.get
 import no.nav.helse.spole.AUTH_NAME
 import no.nav.helse.spole.JsonConfig
 import java.time.LocalDate
 
-class HistorikkTjeneste(val infotrygd: PeriodeKilde,
-                        val spa: PeriodeKilde) {
+class HistorikkTjeneste(
+    val infotrygd: PeriodeKilde,
+    val spa: PeriodeKilde
+) {
 
-    fun hentPerioder(aktørId: AktørId,
-                     fom: LocalDate?): Sykepengeperioder {
+    fun hentPerioder(
+        aktørId: AktørId,
+        fom: LocalDate?
+    ): Sykepengeperioder {
         val faktiskFom = fom ?: LocalDate.now().minusYears(3)
         return infotrygdPerioder(aktørId, faktiskFom).join(spaPerioder(aktørId, faktiskFom))
     }
@@ -30,9 +34,12 @@ interface PeriodeKilde {
 fun Routing.historikk(tjeneste: HistorikkTjeneste) {
     authenticate(AUTH_NAME) {
         get("/sykepengeperioder/{aktorId}") {
-            val perioder =
-                tjeneste.hentPerioder(call.parameters["aktorId"]!!, LocalDate.now().minusYears(3))
+            val optionalDate = call.parameters["fraDato"]
+            val fraDato: LocalDate = optionalDate?.toDate() ?: LocalDate.now().minusYears(3)
+            val perioder = tjeneste.hentPerioder(call.parameters["aktorId"]!!, fraDato)
             call.respond(HttpStatusCode.OK, JsonConfig.accessTokenMapper.writeValueAsBytes(perioder))
         }
     }
 }
+
+private fun String.toDate(): LocalDate = LocalDate.parse(this)
